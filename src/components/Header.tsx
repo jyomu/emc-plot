fb26a57c82305ca37a5ffa3d6455d07e3ba0ab5a
jyomu <developer@jyomu.net>
2025-06-26 14:25:52 +0900

    style: add padding and gap to header for improved layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { GitHubLink } from './GitHubLink'
 
 export const Header: React.FC = () => (
-  <header className="flex justify-between items-center">
+  <header className="flex justify-between items-center px-4 gap-4">
     <div className="flex items-center">
       {/* 左詰め要素をここに追加可能 */}
     </div>
