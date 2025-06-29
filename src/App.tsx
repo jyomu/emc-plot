840d54f445f69cde874fbe817fe30bdba6069b24
jyomu <developer@jyomu.net>
2025-06-29 14:45:33 +0900

    React Queryを追加し、AppコンポーネントでQueryClientProviderを使用するように変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { SParamChart } from './apps/SParamChart'
 import { Header } from './components/ui/Header'
 
 function App() {
+  const queryClient = new QueryClient()
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Header />
       <div className="flex-1 flex flex-col justify-center">
         <SParamChart />
       </div>
-    </>
+    </QueryClientProvider>
   )
 }
 
